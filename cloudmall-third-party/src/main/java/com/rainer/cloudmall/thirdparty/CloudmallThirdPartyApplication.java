f987623d9ff091bdd937d06696f48a3adae192d9
StarRainer <2196632542@qq.com>
2026-01-18 17:59:32 +0800

    build(cloudmall-third-party): 第三方模块初始化
    - 引入了第三方模块
    - 在第三方模块导入阿里云 OSS 依赖

---
+++ cloudmall-third-party/src/main/java/com/rainer/cloudmall/thirdparty/CloudmallThirdPartyApplication.java
@@ -0,0 +1,17 @@
+package com.rainer.cloudmall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients("com.rainer.cloudmall.thirdparty.feign")
+public class CloudmallThirdPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudmallThirdPartyApplication.class, args);
+	}
+
+}
---
+++ cloudmall-third-party/src/test/java/com/rainer/cloudmall/thirdparty/CloudmallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainer.cloudmall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudmallThirdPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
